4e6e74b9be64ba744066ec15afa56fe3243f287f
Steven Le <64396816+StevenLe2012@users.noreply.github.com>
2023-09-11 00:27:09 -0700

    Removed Star Canvas

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ const App = () => {
         {/* <Feedbacks /> */}
         <div className='relative z-0'>
           <Contact />
-          <StarsCanvas />
+          {/* <StarsCanvas /> */}
         </div>
       </div>
 
